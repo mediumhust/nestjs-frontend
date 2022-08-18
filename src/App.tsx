ca2e3ba2b16f6c9fc8b706aad0c05177b56aa0e9
mediumhust <medium.hust.lee@gmail.com>
2022-08-18 17:27:17 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GoogleButton from './GoogleButton';
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleButton />
+    </div>
+  );
+}
+
+export default App;
